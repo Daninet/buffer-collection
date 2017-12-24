6d5a0a98849952d72efdce082bbeaef8c805bbfd
Daniel Biro <danibiro93@gmail.com>
2017-12-24 11:39:35 +0200

    Add buffercollection source

---
+++ BufferCollection.js
@@ -0,0 +1,309 @@
+class BufferCollection {
+  constructor() {
+    this._bufs = [];
+    this._length = 0;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get count() {
+    return this._bufs.length;
+  }
+
+  _getPositionByByteOffset(offset) {
+    let c = 0;
+    let bufCount = this._bufs.length;
+    for (let key = 0; key < bufCount; key++) {
+      if (offset < c + this._bufs[key].length) {
+        return {
+          key,
+          offset: offset - c
+        };
+      }
+      c += this._bufs[key].length;
+      if (c > offset) {
+        return null;
+      }
+    }
+    return null;
+  }
+
+  // gets first buffer from the beginning
+  shiftBuffer () {
+    if (this._bufs.length === 0) {
+      return undefined;
+    }
+    this._length -= this._bufs[0].length;
+    return this._bufs.shift();
+  }
+
+  read (len) {
+    if (len < 1) {
+      return false;
+    }
+    if (this._length < len) {
+      return false;
+    }
+    const position =  this._getPositionByByteOffset(len - 1);
+    const lastKey = position.key;
+    const ret = Buffer.concat(this._bufs, len);
+
+    // remove used buffers
+    if (lastKey > 0) {
+      this._bufs = this._bufs.slice(lastKey + 1);
+    }
+
+    // cut first buffer
+    if (this._bufs.length > 0) {
+      this._bufs[0] = this._bufs[0].slice(position.offset + 1);
+      if (this._bufs[0].length === 0) {
+        this._bufs = this._bufs.slice(1);
+      }
+    }
+
+    this._length -= len;
+
+    return ret;
+  }
+
+  _verifyMatch(bufferValue, bufferIndex, bufferOffset, offset) {
+    // check size
+    if (bufferValue.length > this._length - offset) {
+      return false;
+    }
+
+    let currentBuffer = this._bufs[bufferIndex];
+    let c = 0;
+    for (let i = 0; i < bufferValue.length; i++) {
+      // console.log('for', i);
+      if (bufferOffset + i - c >= currentBuffer.length) {
+        c += currentBuffer.length - bufferOffset;
+        currentBuffer = this._bufs[++bufferIndex];
+        bufferOffset = 0;
+      }
+      if (bufferValue[i] !== currentBuffer[bufferOffset + i - c]) {
+        // console.log('if2', i);
+        return false;
+      }
+    }
+    return true;
+  }
+
+  indexOf(value, startOffset = 0) {
+    if (startOffset >= this._length || startOffset < 0) {
+      return -1;
+    }
+
+    const bufferValue = Buffer.from(value);
+    if (startOffset + bufferValue.length >= this._length) {
+      return -1;
+    }
+
+    const position = this._getPositionByByteOffset(startOffset);
+
+    let globalOffset = startOffset;
+    let offset = position.offset;
+    for (let currentBufferKey = position.key; currentBufferKey < this._bufs.length; currentBufferKey++) {
+      const buf = this._bufs[currentBufferKey];
+      while (offset < buf.length) {
+        const isMatch = this._verifyMatch(bufferValue, currentBufferKey, offset, globalOffset);
+        if (isMatch) {
+          return globalOffset;
+        }
+        offset++;
+        globalOffset++;
+      }
+      offset = 0;
+    }
+    return -1;
+  }
+
+  includes (value, startOffset = 0) {
+    return this.indexOf(value, startOffset) !== -1;
+  }
+
+  fill (value, offset = 0, end = null) {
+    const buf = Buffer.from(value);
+    const startPosition = this._getPositionByByteOffset(offset);
+    const endPosition = end === null ? { key: this._bufs.length - 1, offset: this._length } : this._getPositionByByteOffset(end);
+    let bufOffset = 0;
+    for (let currentBufferKey = startPosition.key; currentBufferKey <= endPosition.key; currentBufferKey++) {
+      const currentBuffer = this._bufs[currentBufferKey];
+      const iStart = currentBufferKey === startPosition.key ? startPosition.offset : 0;
+      const iEnd = currentBufferKey === endPosition.key ? endPosition.offset : this._bufs[currentBufferKey].length;
+      for (let i = iStart; i < iEnd; i++) {
+        this._bufs[currentBufferKey][i] = buf[bufOffset++];
+        if (bufOffset === buf.length) {
+          bufOffset = 0;
+        }
+      }
+    }
+    return this;
+  }
+
+  push (obj) {
+    if (obj instanceof BufferCollection) {  
+      this._bufs.push(...obj._bufs);
+      this._length += obj._length;
+    } else if (obj instanceof Buffer) {
+      this._bufs.push(obj);
+      this._length += obj.length;
+    } else {
+      const buf = Buffer.from(obj);
+      this._bufs.push(buf);
+      this._length += buf.length;
+    }
+    return this;
+  }
+
+  // source is the current buffer
+  copy (target, targetStart, sourceStart, sourceEnd) {
+    Buffer.concat(this._bufs).copy(target, targetStart, sourceStart, sourceEnd);
+    return this;
+  }
+
+  entries () {
+    let key = 0;
+    let offset = 0;
+    let count = 0;
+    let done = false;
+
+    return {
+      next: () => {
+        if (done) {
+          return {
+            value: undefined,
+            done: true,
+          };
+        }
+
+        const res = {
+          value: [count++, this._bufs[key][offset]],
+          done: false,
+        };
+
+        if (offset >= this._bufs[key].length - 1) {
+          offset = 0;
+          key++;
+          if (key === this._bufs.length) {
+            done = true;
+          }
+        } else {
+          offset++;
+        }
+
+        return res;
+      }
+    };
+  }
+
+  keys () {
+    const entries = this.entries();
+    return {
+      next: () => {
+        const it = entries.next();
+        return {
+          value: it.value && it.value[0],
+          done: it.done
+        };
+      }
+    }
+  }
+
+  values() {
+    const entries = this.entries();
+    return {
+      next: () => {
+        const it = entries.next();
+        return {
+          value: it.value && it.value[1],
+          done: it.done
+        };
+      }
+    }
+  }
+
+  // return new instance with the interval (same memory object)
+  slice (start, end) {
+    // create new array with the new instances
+    const arr = [];
+    const startPosition = this._getPositionByByteOffset(start);
+    let endPosition = end !== undefined ? this._getPositionByByteOffset(end) : null;
+    if (endPosition === null) {
+      endPosition = this._getPositionByByteOffset(this._length - 1);
+      endPosition.offset++;
+    }
+
+    if (startPosition.key === endPosition.key) {
+    
+      arr.push(this._bufs[startPosition.key].slice(startPosition.offset, endPosition.offset));
+    
+    } else {
+
+      // start element
+      arr.push(this._bufs[startPosition.key].slice(startPosition.offset));
+      
+      // middle elements
+      for (let key = startPosition.key + 1; key <= endPosition.key - 1; key++) {
+        arr.push(this._bufs[key]);
+      }
+
+      // end element
+      arr.push(this._bufs[endPosition.key].slice(0, endPosition.offset));
+    
+    }
+
+    const newBuf = new BufferCollection();
+    newBuf._bufs = arr;
+    newBuf._length = 1;
+    return newBuf;
+  }
+
+  lastIndexOf (value, offset) {
+
+  }
+
+  readUInt8 (offset, noAssert) {
+    const position = this._getPositionByByteOffset(offset);
+    return this._bufs[position.key].readUInt8(position.offset, noAssert);
+  }
+
+  writeUInt8 (value, offset, noAssert) {
+    const position = this._getPositionByByteOffset(offset);
+    return this._bufs[position.key].writeUInt8(value, position.offset, noAssert);
+  }
+
+  compact () {
+    this._bufs = [Buffer.concat(this._bufs)];
+    return this;
+  }
+
+  equals (cmpBuf) {
+    let pos = 0;
+    if (this._length !== cmpBuf.length) {
+      return false;
+    }
+    for (let key = 0; key < this._bufs.length; key++) {
+      const buf = this._bufs[key];
+      if (!buf.equals(cmpBuf.slice(pos, pos + buf.length))) {
+        return false;
+      }
+      pos += buf.length;
+    }
+    return true;
+    // TODO
+    // return Buffer.concat(this._bufs).equals(buf);
+  }
+
+  toString () {
+    return this._bufs.map((buf) => buf.toString()).join('');
+  }
+
+  [Symbol.iterator] () {
+    return this.values();
+  }
+}
+
+module.exports = BufferCollection;
---
+++ BufferCollection.test.js
@@ -0,0 +1,378 @@
+const BufferCollection = require('./BufferCollection');
+
+test('get-position-by-byte-offset', () => {
+  const buf = new BufferCollection();
+  expect(buf._getPositionByByteOffset(0)).toBe(null);
+  buf.push('abcd');
+  buf.push('x');
+  buf.push('y');
+  buf.push('abcd');
+  expect(buf._getPositionByByteOffset(0)).toEqual({key: 0, offset: 0});
+  expect(buf._getPositionByByteOffset(3)).toEqual({key: 0, offset: 3});
+  expect(buf._getPositionByByteOffset(4)).toEqual({key: 1, offset: 0});
+  expect(buf._getPositionByByteOffset(5)).toEqual({key: 2, offset: 0});
+  expect(buf._getPositionByByteOffset(6)).toEqual({key: 3, offset: 0});
+  expect(buf._getPositionByByteOffset(7)).toEqual({key: 3, offset: 1});
+  expect(buf._getPositionByByteOffset(9)).toEqual({key: 3, offset: 3});
+  expect(buf._getPositionByByteOffset(10)).toBe(null);
+  const buf2 = new BufferCollection();
+  buf2.push('x');
+  expect(buf2._getPositionByByteOffset(0)).toEqual({key: 0, offset: 0});
+  expect(buf2._getPositionByByteOffset(1)).toBe(null);
+});
+
+
+test('simple-read', () => {
+  const buf = new BufferCollection();
+  buf.push('abcd');
+  expect(buf.read(0)).toBe(false);
+  expect(buf.read(1).toString()).toBe('a');
+  expect(buf.read(1).toString()).toBe('b');
+  expect(buf.read(1).toString()).toBe('c');
+  expect(buf.read(1).toString()).toBe('d');
+  expect(buf.read(1)).toBe(false);
+});
+
+test('complex-read', () => {
+  const buf = new BufferCollection();
+  buf.push('abcd');
+  expect(buf.read(1).toString()).toBe('a');
+  expect(buf.length).toBe(3);
+  expect(buf.count).toBe(1);
+  expect(buf.read(1).toString()).toBe('b');
+  expect(buf.length).toBe(2);
+  expect(buf.count).toBe(1);
+  expect(buf.read(3)).toBe(false);
+  expect(buf.length).toBe(2);
+  expect(buf.count).toBe(1);
+  buf.push('e');
+  buf.push('fgh');
+  buf.push('i');
+  expect(buf.length).toBe(7);
+  expect(buf.count).toBe(4);
+  expect(buf.read(8)).toBe(false);
+  expect(buf.read(7).toString()).toBe('cdefghi');
+  expect(buf.length).toBe(0);
+  expect(buf.count).toBe(0);
+  expect(buf.read(1)).toBe(false);
+  buf.push('x');
+  expect(buf.read(1).toString()).toBe('x');
+  expect(buf.length).toBe(0);
+  expect(buf.count).toBe(0);
+});
+
+test('simple-push', () => {
+  const buf = new BufferCollection();
+  expect(buf.length).toBe(0);
+  expect(buf.count).toBe(0);
+  buf.push('abcdef');
+  expect(buf.length).toBe(6);
+  expect(buf.count).toBe(1);
+  buf.push('g');
+  expect(buf.length).toBe(7);
+  expect(buf.count).toBe(2);
+  expect(buf.read(7).toString()).toBe('abcdefg');
+  expect(buf.length).toBe(0);
+  expect(buf.count).toBe(0);
+});
+
+test('shift-buffer', () => {
+  const buf = new BufferCollection();
+  buf.push('abcdef');
+  buf.push('g');
+  buf.push('123');
+  expect(buf.shiftBuffer().toString()).toBe('abcdef');
+  expect(buf.count).toBe(2);
+  expect(buf.shiftBuffer().toString()).toBe('g');
+  expect(buf.count).toBe(1);
+  expect(buf.shiftBuffer().toString()).toBe('123');
+  expect(buf.count).toBe(0);
+  expect(buf.shiftBuffer()).toBe(undefined);
+  expect(buf.count).toBe(0);
+});
+
+test('push-types', () => {
+  const buf = new BufferCollection();
+  buf.push(Buffer.from('abcdef'));
+  expect(buf.length).toBe(6);
+  expect(buf.count).toBe(1);
+  expect(buf.toString()).toBe('abcdef');
+  buf.push('g');
+  expect(buf.length).toBe(7);
+  expect(buf.count).toBe(2);
+  expect(buf.toString()).toBe('abcdefg');
+  const buf2 = new BufferCollection();
+  buf2.push('12');
+  buf2.push('3');
+  buf2.push('456');
+  buf.push(buf2);
+  expect(buf.length).toBe(13);
+  expect(buf.count).toBe(5);
+  expect(buf.toString()).toBe('abcdefg123456');
+});
+
+test('verify-match', () => {
+  const buf = new BufferCollection();
+  buf.push('abcdef');
+  buf.push('g');
+  expect(buf._verifyMatch(Buffer.from('cd'), 0, 2, 2)).toBe(true);
+  expect(buf._verifyMatch(Buffer.from('cdef'), 0, 2, 2)).toBe(true);
+  expect(buf._verifyMatch(Buffer.from('cdefg'), 0, 2, 2)).toBe(true);
+  expect(buf._verifyMatch(Buffer.from('cd'), 0, 1, 2)).toBe(false);
+  expect(buf._verifyMatch(Buffer.from('g'), 1, 0, 6)).toBe(true);
+  buf.push('h12345');
+  expect(buf._verifyMatch(Buffer.from('cdefgh12'), 0, 2, 2)).toBe(true);
+  expect(buf._verifyMatch(Buffer.from('cdefg112'), 0, 2, 2)).toBe(false);
+  expect(buf._verifyMatch(Buffer.from('cdefgh12345'), 0, 2, 2)).toBe(true);
+  expect(buf._verifyMatch(Buffer.from('cdefgh123456asdfasdf'), 0, 2, 2)).toBe(false);
+});
+
+
+test('index-of-simple', () => {
+  const buf = new BufferCollection();
+  buf.push('abcd');
+  expect(buf.indexOf('x')).toBe(-1);
+  expect(buf.indexOf('a')).toBe(0);
+  expect(buf.indexOf('d')).toBe(3);
+  expect(buf.indexOf('ac')).toBe(-1);
+  expect(buf.indexOf('cd')).toBe(2);
+  expect(buf.indexOf('cda')).toBe(-1);
+  buf.push('xdfdf');
+  expect(buf.indexOf('dx')).toBe(3);
+  expect(buf.indexOf('dxdfdf')).toBe(3);
+  expect(buf.indexOf('dxdfdfx')).toBe(-1);
+  expect(buf.indexOf('xdfdf')).toBe(4);
+  buf.push('u');
+  buf.push('v');
+  buf.push('z');
+  buf.push('1');
+  expect(buf.indexOf('fuvz')).toBe(8);
+  expect(buf.indexOf('fuvz1')).toBe(8);
+  expect(buf.indexOf('fuvz11')).toBe(-1);
+});
+
+test('index-of-bad-param', () => {
+  const buf = new BufferCollection();
+  buf.push('abcd');
+  expect(buf.indexOf('abcdabcd')).toBe(-1);
+  expect(buf.indexOf('a', -1)).toBe(-1);
+  expect(buf.indexOf('a', 1)).toBe(-1);
+  expect(buf.indexOf('a', 4)).toBe(-1);
+});
+
+test('index-of-position', () => {
+  const buf = new BufferCollection();
+  buf.push('abcdxy');
+  buf.push('x');
+  buf.push('y');
+  buf.push('abcd');
+  expect(buf.indexOf('cd')).toBe(2);
+  expect(buf.indexOf('cd', 3)).toBe(10);
+  expect(buf.indexOf('xya', 0)).toBe(6);
+  expect(buf.indexOf('xya', 1)).toBe(6);
+  expect(buf.indexOf('xy', 1)).toBe(4);
+  expect(buf.indexOf('xy', 3)).toBe(4);
+  expect(buf.indexOf('xy', 4)).toBe(4);
+  expect(buf.indexOf('xy', 5)).toBe(6);
+  expect(buf.indexOf('xy', 7)).toBe(-1);
+});
+
+test('includes', () => {
+  const buf = new BufferCollection();
+  buf.push('abcd');
+  expect(buf.includes('x')).toBe(false);
+  expect(buf.includes('a')).toBe(true);
+  expect(buf.includes('d')).toBe(true);
+  expect(buf.includes('ac')).toBe(false);
+  expect(buf.includes('cd')).toBe(true);
+  expect(buf.includes('cda')).toBe(false);
+  buf.push('xdfdf');
+  expect(buf.includes('dx')).toBe(true);
+  expect(buf.includes('dxdfdf')).toBe(true);
+  expect(buf.includes('dxdfdfx')).toBe(false);
+  expect(buf.includes('xdfdf')).toBe(true);
+  buf.push('u');
+  buf.push('v');
+  buf.push('z');
+  buf.push('1');
+  expect(buf.includes('fuvz')).toBe(true);
+  expect(buf.includes('fuvz1')).toBe(true);
+  expect(buf.includes('fuvz11')).toBe(false);
+});
+
+test('compact', () => {
+  const buf = new BufferCollection();
+  buf.push('abcd');
+  buf.push('x');
+  buf.push('y');
+  expect(buf.length).toBe(6);
+  expect(buf.count).toBe(3);
+  buf.compact();
+  expect(buf.length).toBe(6);
+  expect(buf.count).toBe(1);
+  expect(buf.read(6).toString()).toBe('abcdxy');
+});
+
+test('fill', () => {
+  const buf = new BufferCollection();
+  buf.push('abcd');
+  buf.push('x');
+  buf.push('y');
+  buf.push('12345678');
+  buf.fill('0');
+  expect(buf.length).toBe(14);
+  expect(buf.count).toBe(4);
+  expect(buf.toString()).toBe('00000000000000');
+  buf.fill('abc');
+  expect(buf.toString()).toBe('abcabcabcabcab');
+  buf.fill('abcabcabcabcabcabcabcabc');
+  expect(buf.toString()).toBe('abcabcabcabcab');
+  buf.fill('12345', 2);
+  expect(buf.toString()).toBe('ab123451234512');
+  buf.fill('xy', 4, 9);
+  expect(buf.toString()).toBe('ab12xyxyx34512');
+});
+
+test('copy', () => {
+  const buf = new BufferCollection();
+  buf.push('abcd');
+  buf.push('x');
+  buf.push('y');
+  buf.push('12345678');
+  const buf2 = Buffer.alloc(15).fill('o');
+  buf.copy(buf2, 2, 3, 11);
+  expect(buf2.toString()).toBe('oodxy12345ooooo');
+});
+
+test('slice', () => {
+  const buf = new BufferCollection();
+  buf.push('abcd');
+  buf.push('x');
+  buf.push('y');
+  buf.push('123');
+  expect(buf.slice(0, 0).toString()).toBe('');
+  expect(buf.slice(0, 1).toString()).toBe('a');
+  expect(buf.slice(0, 9).toString()).toBe('abcdxy123');
+  expect(buf.slice(0).toString()).toBe('abcdxy123');
+  expect(buf.slice(2, 3).toString()).toBe('c');
+  expect(buf.slice(5, 7).toString()).toBe('y1');
+  expect(buf.slice(3).toString()).toBe('dxy123');
+});
+
+test('entries', () => {
+  const buf = new BufferCollection();
+  buf.push('abc');
+  buf.push('x');
+  buf.push('12');
+  const entries = buf.entries();
+  expect(entries.next()).toEqual({value: [0, 'a'.charCodeAt(0)], done: false});
+  expect(entries.next()).toEqual({value: [1, 'b'.charCodeAt(0)], done: false});
+  expect(entries.next()).toEqual({value: [2, 'c'.charCodeAt(0)], done: false});
+  expect(entries.next()).toEqual({value: [3, 'x'.charCodeAt(0)], done: false});
+  expect(entries.next()).toEqual({value: [4, '1'.charCodeAt(0)], done: false});
+  expect(entries.next()).toEqual({value: [5, '2'.charCodeAt(0)], done: false});
+  expect(entries.next()).toEqual({value: undefined, done: true});
+});
+
+test('keys', () => {
+  const buf = new BufferCollection();
+  buf.push('abc');
+  buf.push('x');
+  buf.push('12');
+  const entries = buf.keys();
+  expect(entries.next()).toEqual({value: 0, done: false});
+  expect(entries.next()).toEqual({value: 1, done: false});
+  expect(entries.next()).toEqual({value: 2, done: false});
+  expect(entries.next()).toEqual({value: 3, done: false});
+  expect(entries.next()).toEqual({value: 4, done: false});
+  expect(entries.next()).toEqual({value: 5, done: false});
+  expect(entries.next()).toEqual({value: undefined, done: true});
+});
+
+test('values', () => {
+  const buf = new BufferCollection();
+  buf.push('abc');
+  buf.push('x');
+  buf.push('12');
+  const entries = buf.values();
+  expect(entries.next()).toEqual({value: 'a'.charCodeAt(0), done: false});
+  expect(entries.next()).toEqual({value: 'b'.charCodeAt(0), done: false});
+  expect(entries.next()).toEqual({value: 'c'.charCodeAt(0), done: false});
+  expect(entries.next()).toEqual({value: 'x'.charCodeAt(0), done: false});
+  expect(entries.next()).toEqual({value: '1'.charCodeAt(0), done: false});
+  expect(entries.next()).toEqual({value: '2'.charCodeAt(0), done: false});
+  expect(entries.next()).toEqual({value: undefined, done: true});
+});
+
+test('equals', () => {
+  const buf = new BufferCollection();
+  buf.push('abcd');
+  buf.push('x');
+  buf.push('y');
+  expect(buf.length).toBe(6);
+  expect(buf.count).toBe(3);
+  expect(buf.equals(Buffer.from('abcdxy'))).toBe(true);
+  expect(buf.equals(Buffer.from('adcdxy'))).toBe(false);
+  expect(buf.equals(Buffer.from('abcdxy1'))).toBe(false);
+  expect(buf.equals(Buffer.from('a'))).toBe(false);
+  expect(buf.length).toBe(6);
+  expect(buf.count).toBe(3);
+});
+
+test('toString', () => {
+  const buf = new BufferCollection();
+  buf.push('abcd');
+  buf.push('x');
+  buf.push('y');
+  buf.push('abcd1234');
+  expect(buf.toString()).toBe('abcdxyabcd1234');
+});
+
+test('readUInt8', () => {
+  const b1 = Buffer.from([255, 240]);
+  const b2 = Buffer.from([1]);
+  const b3 = Buffer.from([3, 9, 7]);
+  const buf = new BufferCollection();
+  buf.push(b1);
+  buf.push(b2);
+  buf.push(b3);
+  expect(buf.readUInt8(0)).toBe(255);
+  expect(buf.readUInt8(1)).toBe(240);
+  expect(buf.readUInt8(2)).toBe(1);
+  expect(buf.readUInt8(3)).toBe(3);
+  expect(buf.readUInt8(4)).toBe(9);
+  expect(buf.readUInt8(5)).toBe(7);
+});
+
+test('writeUInt8', () => {
+  const b1 = Buffer.from('ab');
+  const b2 = Buffer.from('c');
+  const b3 = Buffer.from('def');
+  const buf = new BufferCollection();
+  buf.push(b1);
+  buf.push(b2);
+  buf.push(b3);
+  buf.writeUInt8('1'.charCodeAt(0), 0);
+  buf.writeUInt8('2'.charCodeAt(0), 1);
+  buf.writeUInt8('3'.charCodeAt(0), 2);
+  buf.writeUInt8('4'.charCodeAt(0), 3);
+  buf.writeUInt8('5'.charCodeAt(0), 4);
+  buf.writeUInt8('6'.charCodeAt(0), 5);
+  expect(buf.length).toBe(6);
+  expect(buf.count).toBe(3);
+  expect(buf.toString()).toBe('123456');
+});
+
+test('iterator', () => {
+  const buf = new BufferCollection();
+  buf.push('abcd');
+  buf.push('x');
+  buf.push('y');
+  buf.push('abcd1234');
+  const arr = [];
+  for (const value of buf) {
+    arr.push(String.fromCharCode(value));
+  }
+  expect(arr.length).toBe(14);
+  expect(arr.join('')).toBe('abcdxyabcd1234');
+});
